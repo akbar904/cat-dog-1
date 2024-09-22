f2327a171e2af9b362440fcaadeab4c12bf2946c
zharfan-a <zharfan@walturn.com>
2024-09-22 23:47:23 +0700

    Update 15 files

--- src/lib/models/bird.dart
+++ src/lib/models/bird.dart
@@ -1,32 +1,33 @@
+
 import 'package:equatable/equatable.dart';
 
 class Bird extends Equatable {
-  final String name;
-  final String species;
-  final int age;
+	final String name;
+	final String species;
+	final int age;
 
-  const Bird({
-    required this.name,
-    required this.species,
-    required this.age,
-  });
+	const Bird({
+		required this.name,
+		required this.species,
+		required this.age,
+	});
 
-  factory Bird.fromJson(Map<String, dynamic> json) {
-    return Bird(
-      name: json['name'] as String,
-      species: json['species'] as String,
-      age: json['age'] as int,
-    );
-  }
+	factory Bird.fromJson(Map<String, dynamic> json) {
+		return Bird(
+			name: json['name'] as String,
+			species: json['species'] as String,
+			age: json['age'] as int,
+		);
+	}
 
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'species': species,
-      'age': age,
-    };
-  }
+	Map<String, dynamic> toJson() {
+		return {
+			'name': name,
+			'species': species,
+			'age': age,
+		};
+	}
 
-  @override
-  List<Object?> get props => [name, species, age];
+	@override
+	List<Object?> get props => [name, species, age];
 }
