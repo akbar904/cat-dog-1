33bc78298d26f693c9a1e60edc12863c44fac36f
zharfan-a <zharfan@walturn.com>
2024-09-22 23:47:28 +0700

    Update 15 files

--- src/lib/widgets/bird_item_widget.dart
+++ src/lib/widgets/bird_item_widget.dart
@@ -1,22 +1,45 @@
+
 import 'package:flutter/material.dart';
 import 'package:bird_app/models/bird.dart';
 
-class BirdItemWidget extends StatelessWidget {
-  final Bird bird;
+class BirdItemWidget extends StatefulWidget {
+	final Bird bird;
 
-  const BirdItemWidget({Key? key, required this.bird}) : super(key: key);
+	const BirdItemWidget({Key? key, required this.bird}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(bird.name),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(bird.species),
-          Text('Age: ${bird.age}'),
-        ],
-      ),
-    );
-  }
+	@override
+	_BirdItemWidgetState createState() => _BirdItemWidgetState();
+}
+
+class _BirdItemWidgetState extends State<BirdItemWidget> {
+	bool _isBird = true;
+
+	@override
+	Widget build(BuildContext context) {
+		return ListTile(
+			title: Text(widget.bird.name),
+			subtitle: Column(
+				crossAxisAlignment: CrossAxisAlignment.start,
+				children: [
+					Text(widget.bird.species),
+					Text('Age: ${widget.bird.age}'),
+				],
+			),
+			trailing: GestureDetector(
+				onTap: () {
+					setState(() {
+						_isBird = !_isBird;
+					});
+				},
+				child: Row(
+					mainAxisSize: MainAxisSize.min,
+					children: [
+						Icon(_isBird ? Icons.access_time : Icons.person),
+						SizedBox(width: 5),
+						Text(_isBird ? 'Bird' : 'Dog'),
+					],
+				),
+			),
+		);
+	}
 }
--- src/lib/widgets/bird_list_widget.dart
+++ src/lib/widgets/bird_list_widget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubits/bird_cubit.dart';
@@ -5,26 +6,26 @@ import 'bird_item_widget.dart';
 
 // BirdListWidget
 class BirdListWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<BirdCubit, BirdState>(
-      builder: (context, state) {
-        if (state is BirdLoadingState) {
-          return Center(child: CircularProgressIndicator());
-        } else if (state is BirdLoadedState) {
-          return ListView.builder(
-            itemCount: state.birds.length,
-            itemBuilder: (context, index) {
-              final bird = state.birds[index];
-              return BirdItemWidget(bird: bird);
-            },
-          );
-        } else if (state is BirdErrorState) {
-          return Center(child: Text(state.message));
-        } else {
-          return Center(child: Text('Unknown state'));
-        }
-      },
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return BlocBuilder<BirdCubit, BirdState>(
+			builder: (context, state) {
+				if (state is BirdLoadingState) {
+					return Center(child: CircularProgressIndicator());
+				} else if (state is BirdLoadedState) {
+					return ListView.builder(
+						itemCount: state.birds.length,
+						itemBuilder: (context, index) {
+							final bird = state.birds[index];
+							return BirdItemWidget(bird: bird);
+						},
+					);
+				} else if (state is BirdErrorState) {
+					return Center(child: Text(state.message));
+				} else {
+					return Center(child: Text('Unknown state'));
+				}
+			},
+		);
+	}
 }
